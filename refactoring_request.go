64c8c59296851b0868ed2dd979a43969e59e8563
Kazuhiro Oinuma <oinume@gmail.com>
2024-08-31 23:56:14 +0900

    Split types.go to refactoring_request.go and refactoring_target.go

---
+++ refactoring_request.go
@@ -0,0 +1,59 @@
+package corefactorer
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+	"text/template"
+)
+
+type PullRequest struct {
+	Title string
+	Body  string
+	Diff  string
+}
+
+type TargetFile struct {
+	Path    string
+	Content string
+}
+
+//go:embed prompt.template
+var promptTemplate string
+
+type RefactoringRequest struct {
+	PullRequests []*PullRequest
+	TargetFiles  []*TargetFile
+	Prompt       string
+}
+
+func (rr *RefactoringRequest) CreatePrompt() (string, error) {
+	var sb strings.Builder
+	t, err := template.New("prompt").Parse(promptTemplate)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse template: %w", err)
+	}
+
+	paths := make([]string, 0, len(rr.TargetFiles))
+	for _, tf := range rr.TargetFiles {
+		paths = append(paths, tf.Path)
+	}
+	data := struct {
+		Diff        string
+		TargetFiles []*TargetFile
+		TargetPaths string
+	}{
+		Diff:        rr.PullRequests[0].Diff,
+		TargetFiles: rr.TargetFiles,
+		TargetPaths: strings.Join(paths, ", "),
+	}
+	if err := t.Execute(&sb, &data); err != nil {
+		return "", fmt.Errorf("failed to template execute: %w", err)
+	}
+
+	return sb.String(), nil
+}
+
+type RefactoringResult struct {
+	RawContent string
+}
